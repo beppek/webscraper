c4fbf9370ebd0c16e5f85c9637861290f66b5e54
beppek <beppe@live.se>
2017-11-12 21:08:42 +0100

    Break out code

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Crawler {
+/**
+ * Crawler class
+ * crawls the wikipedia pages supplied in the constructor
+ * Initiate with start
+ * */
+class Crawler {
+
+    private Set<String> visited = new HashSet<>();
+    private WikiPage[] pages;
+
+    Crawler(WikiPage[] pages) {
+        this.pages = pages;
+    }
+
+    void start() {
+        WikipediaScraper s = new WikipediaScraper();
+        Set<String> links = new HashSet<>();
+        for (WikiPage page : pages) {
+            page.setRootPage(page.getTitle());
+            try {
+                s.scrape(page);
+                links.addAll(crawlLinks(page));
+                PageFileWriter pw = new PageFileWriter(page);
+                pw.savePage();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("The crawler visited " + visited.size() + " pages, and extracted " + links.size() + " links.");
+    }
+
+    /**
+     * Crawls the links from the root page down 2 levels
+     * */
+    private Set<String> crawlLinks(WikiPage root) throws Exception {
+        int depth = 2;
+        WikipediaScraper s = new WikipediaScraper();
+        Set<String> links = root.getLinks();
+        Set<String> pageLinks = new HashSet<>();
+        pageLinks.addAll(links);
+        for (int i = 0; i < depth; i++) {
+            for (String link : pageLinks) {
+                if (!visited.contains(link)) {
+                    visited.add(link);
+                    WikiPage page = new WikiPage(link);
+                    page.setRootPage(root.getTitle());
+                    s.scrape(page);
+                    links.addAll(page.getLinks());
+                    PageFileWriter pw = new PageFileWriter(page);
+                    pw.savePage();
+                }
+            }
+            pageLinks = new HashSet<>();
+            pageLinks.addAll(links);
+        }
+        System.out.println("Finished crawling wiki/" + root.getTitle() + " and linked pages 2 levels deep.");
+        return links;
+    }
 }
--- src/PageFileWriter.java
+++ src/PageFileWriter.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
 
-public class FileWriter {
+/**
+ * This class saves the contents of the page on file.
+ * It saves one file for the raw html content, one for text content, one for links and one for the words of the article
+ * */
+class PageFileWriter {
+
+    private WikiPage page;
+    private String[] paths = {"html", "content", "links", "words"};
+
+    PageFileWriter(WikiPage page) {
+        this.page = page;
+    }
+
+    void savePage() {
+        for (String path : paths) {
+            saveFile(path);
+        }
+    }
+
+    private void saveFile(String path) {
+        String dirPath = "data/" + path + "/" + page.getRootPage() + "/";
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File file = new File(dirPath + page.getTitle() + ".txt");
+        try {
+            if (!file.exists()) {
+
+                file.createNewFile();
+            }
+            java.io.FileWriter fw = new java.io.FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            switch (path) {
+                case "html":
+                    bw.write(page.getRawHTML());
+                    break;
+                case "content":
+                    bw.write(page.getTextContent());
+                    break;
+                case "words":
+                    bw.write(page.getBagOfWords());
+                    break;
+                case "links":
+                    for (String link : page.getLinks()) {
+                        bw.write(link);
+                        bw.newLine();
+                    }
+                    break;
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("An error occured while creating this file:");
+            System.out.println(page.getTitle());
+            System.out.println(file.getAbsolutePath());
+            e.printStackTrace();
+        }
+    }
 }
